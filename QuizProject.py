c4642ab8b845775c7f7fce50998339da84b36644
fifiarisandi <fifi_arisandi@yahoo.com>
2017-05-17 14:49:37 +0800

    Add sample text and list of correct answers.

---
+++ QuizProject.py
@@ -0,0 +1,11 @@
+sample = '''You put your right hand __1__, you take your right hand __2__, 
+			you put your right hand __1__, and you __3__ it all about.
+			You do the __4__ pokey, and you __5__ yourself around.
+			That's what it's all about!'''
+
+answers = ["in", "out", "shake", "hokey", "turn"]
+
+
+
+
+
