59f13aa289eba8b09fe3d32d180941b1da351d7f
fifiarisandi <fifi_arisandi@yahoo.com>
2017-05-18 23:50:21 +0800

    Easy level works well, but not yet add the more than once wrong answers feature.

--- QuizProject.py
+++ QuizProject.py
@@ -1,13 +1,13 @@
 from random import randint
 
 easy_song = '''You put your right hand __1__, you take your right hand __2__, 
-			you put your right hand __1__, and you __3__ it all about.
-			You do the __4__ pokey, and you __5__ yourself around.
-			That's what it's all about!'''
+you put your right hand __1__, and you __3__ it all about.
+You do the __4__ pokey, and you __5__ yourself around.
+That's what it's all about!'''
 
 easy_answers = ["in", "out", "shake", "hokey", "turn"]
 #amateur_answers = 
-levels = ["easy-peasy", "amateur", "pro-wannabe", "song-master"]
+levels = ["easy-peasy", "amateur", "almost pro", "the song-master"]
 
 #def random_level():
 #    random_num = randint(0,3)
@@ -20,14 +20,14 @@ levels = ["easy-peasy", "amateur", "pro-wannabe", "song-master"]
 #    else:
 #    	return "song-master"
 
-def random_answer():
-	random_num = randint(0,2)
-	if random_num == 0:
-		return "in"
-	elif random_num == 1:
-	    return "out"
-	else:
-		return "shake"
+#def random_answer():
+#	random_num = randint(0,2)
+#	if random_num == 0:
+#		return "in"
+#	elif random_num == 1:
+#	    return "out"
+#	else:
+#		return "shake"
 
 def fill_in_the_blanks(level,sequence):
     global easy_song
@@ -39,48 +39,36 @@ def fill_in_the_blanks(level,sequence):
         	replacement = "__" + str(sequence) + "__"
         	word = word.replace(replacement, easy_answers[sequence-1])
         	replaced.append(word)
-            #if str(sequence) in word:
-            #    word=word.replace(word, easy_answers[sequence-1])
-            #replaced.append(word)
-            #else:
-            #    replaced.append(word)
         easy_song=" ".join(replaced)
         return easy_song
 
 def easy(sequence):
 	if sequence==0:
-		print "You chose easy-peasy. Don't worry if you can't guess it right the first time, you have 3 chances to do it. Let's sing along! \n" + easy_song + "\n" + "What is the answer for__1__?"
-		#answer=random_answer()
-		answer="in"
+		answer = raw_input("You chose easy-peasy! Don't worry if you can't guess it right the first time, you have 3 chances to do it. Let's sing along! \n\n" + easy_song + "\n\n" + "What is your answer for__1__? ")
 		if answer==easy_answers[sequence]:
 			return sequence+1
 		else:
 			return sequence
 	elif sequence==1:
-		print easy_song + "\n" + "What is the answer for __2__?"
-		#answer=random_answer()
-		answer="out"
+		answer = raw_input("\nWhat is your answer for __2__? ")
 		if answer==easy_answers[sequence]:
 			return sequence+1
 		else:
 			return sequence
 	elif sequence==2:
-	    print easy_song + "\n" + "What is the answer for __3__?"
-	    answer="shake"
+	    answer = raw_input("\nWhat is your answer for __3__? ")
 	    if answer==easy_answers[sequence]:
 	        return sequence+1
 	    else:
 	        return sequence
 	elif sequence==3:
-	    print easy_song + "\n" + "What is the answer for __4__?"
-	    answer="hokey"
+	    answer = raw_input("\nWhat is your answer for __4__? ")
 	    if answer==easy_answers[sequence]:
 	        return sequence+1
 	    else:
 	        return sequence
 	elif sequence==4:
-	    print easy_song + "\n" + "What is the answer for __5__?"
-	    answer="turn"
+	    answer = raw_input("\nWhat is your answer for __5__? ")
 	    if answer==easy_answers[sequence]:
 	        return sequence+1
 	    else:
@@ -88,38 +76,37 @@ def easy(sequence):
 
 def lets_sing_along():
 	sequence=0
-	#level=random_level
-	level="easy-peasy"
+	level = raw_input("Let's sing along! \n\nChoose your level: \n easy-peasy \n amateur \n almost pro \n the song-master\n\n")
 	if level=="easy-peasy":
 		new_sequence=easy(sequence)
 		if new_sequence==sequence:
-			return "That's not the right word, sorry!"
+			return "That's not the right lyric, sorry!"
 		else:
-			print "You got that right!" + "\n" + fill_in_the_blanks(level,new_sequence)
+			print "You got that right!" + "\n\n" + fill_in_the_blanks(level,new_sequence)
 			sequence=new_sequence
 			new_sequence=easy(sequence)
 			if new_sequence==sequence:
-				return "That's not the right word, sorry!"
+				return "That's not the right lyric, sorry!"
 			else:
-			    print "You got that right!" + "\n" + fill_in_the_blanks(level, new_sequence)
+			    print "You got that right!" + "\n\n" + fill_in_the_blanks(level, new_sequence)
 			    sequence=new_sequence
 			    new_sequence=easy(sequence)
 			    if new_sequence==sequence:
-			        return "That's not the right word, sorry!"
+			        return "That's not the right lyric, sorry!"
 			    else:
-			        print "You got that right!" + "\n" + fill_in_the_blanks(level, new_sequence)
+			        print "You got that right!" + "\n\n" + fill_in_the_blanks(level, new_sequence)
 			        sequence=new_sequence
 			        new_sequence=easy(sequence)
 			        if new_sequence==sequence:
-			            return "That's not the right word, sorry!"
+			            return "That's not the right lyric, sorry!"
 			        else:
-			            print "You got that right!" + "\n" + fill_in_the_blanks(level, new_sequence)
+			            print "You got that right!" + "\n\n" + fill_in_the_blanks(level, new_sequence)
 			            sequence=new_sequence
 			            new_sequence=easy(sequence)
 			            if new_sequence==sequence:
-			                return "That's not the right word, sorry!"
+			                return "That's not the right lyric, sorry!"
 			            else:
-			                return "Congratulations! You did it as good as my 4 years-old niece did! Let's sing along!" +"\n" + fill_in_the_blanks(level, new_sequence)
+			                return "Congratulations! You did it as good as my 4 years-old niece did! Let's sing along!" +"\n\n" + fill_in_the_blanks(level, new_sequence)
 
 
 print lets_sing_along()
